68d89193f0e1b825683297f32fa001deda0d9154
A Abhilash <abhi.aecs@gmail.com>
2019-05-27 22:56:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,6 +99,7 @@ if(document.getElementById('rollbackNo').checked ){
     document.getElementById('myTable').rows[i].cells[4].innerHTML = document.getElementById('devpoc').value;
   }
   document.getElementById('dwn').style.display="none";
+   document.getElementById('export2').style.display="block";
 }
 
 /* When the user clicks on the button,
